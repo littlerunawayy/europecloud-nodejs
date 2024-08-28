e6679ed118c2f6ebae76b28bdb46fcc7351274ba
littlerunawayy <85502391+littlerunawayy@users.noreply.github.com>
2024-08-28 22:39:19 +0300

    Upload nodejs files

---
+++ nodeapp.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://master:27017,slave:27017,arbeiter:27017/nodejsapp?replicaSet=rs0',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const Item = require('./models/Item');
+
+app.get('/', (req, res) => {
+  Item.find()
+    .then(items => res.render('index', { items }))
+    .catch(err => res.status(404).json({ msg: 'No items found' }));
+});
+
+app.post('/item/add', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.redirect('/'));
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log('Server running...'));
